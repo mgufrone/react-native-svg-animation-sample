412d50c5b44cbec511c4a5c8c178d090dfc60444
Mochamad Gufron <mgufronefendi@gmail.com>
2017-08-29 17:34:59 +0700

    update colorscheme

--- App/Screens/Power/PowerScreen.js
+++ App/Screens/Power/PowerScreen.js
@@ -93,15 +93,47 @@ function convertWattHour(length) {
   return (length * 1000) / (Math.PI * 2);
 }
 
+function toColor(dec) {
+  return `#${(dec).toString(16)}`;
+}
+
+function colorToDec(hex) {
+  return parseInt(hex.replace('#', '0x'), 0);
+}
+function interpolate(begin, end, step, max) {
+  if (begin < end) {
+    return ((end - begin) * (step / max)) + begin;
+  }
+  return ((begin - end) * (1 - (step / max))) + end;
+}
+function toRgb(hex) {
+  return {
+    r: (hex & 0xff0000) >> 16,
+    g: (hex & 0x00ff00) >> 8,
+    b: (hex & 0x0000ff) >> 0,
+  };
+}
+function interpolateColors(begin, end, steps) {
+  const { r: r1, g: g1, b: b1 } = toRgb(begin);
+  const { r: r2, g: g2, b: b2 } = toRgb(end);
+  return new Array(steps).fill().map((_, step) => {
+    const newR = Math.ceil(interpolate(r1, r2, step, steps));
+    const newG = Math.ceil(interpolate(g1, g2, step, steps));
+    const newB = Math.ceil(interpolate(b1, b2, step, steps));
+    return `rgb(${newR}, ${newG}, ${newB})`;
+  });
+}
+
 export default class PowerScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       startAngle: 0,
-      angleLength: (Math.PI * 1) * (100 / 1000),
+      angleLength: (Math.PI * 2) * (100 / 1000),
     };
     this.onTimeUpdate = this.onTimeUpdate.bind(this);
     this.onUpdate = this.onUpdate.bind(this);
+    this.colors = interpolateColors(colorToDec('#509E2F'), colorToDec('#DA3E00'), 100);
   }
   onTimeUpdate(fromTimeInMinutes, minutesLong) {
     this.setState({ minutesLong });
@@ -113,6 +145,11 @@ export default class PowerScreen extends Component {
       angleLength,
     });
   }
+
+  colorEnd() {
+    return this.colors[Math.ceil(convertWattHour(this.state.angleLength) / 10)];
+  }
+
 
   render() {
     const { angleLength, startAngle } = this.state;
@@ -139,8 +176,8 @@ export default class PowerScreen extends Component {
             strokeWidth={40}
             radius={130}
             gradientColorFrom="#509E2F"
-            gradientColorTo="#DA3E00"
-            bgCircleColor="#0a3678"
+            gradientColorTo={this.colorEnd()}
+            bgCircleColor="#0A3678"
             stopIcon={null}
             startIcon={null}
           />
